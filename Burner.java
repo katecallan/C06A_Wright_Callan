fe8dade1ab27d9350025fce16a996b9e78938109
katecallan <kate_callan@mines.edu>
2025-01-28 13:29:04 -0700

    Added minusButton and updateTemperature

--- Burner.java
+++ Burner.java
@@ -17,26 +17,67 @@ public class Burner{
 
 
 	public Temperature getMyTemperature() {
-        return myTemperature;
-    }
+		return myTemperature;
+	}
 
-    public void plusButton() { // Increases the setting
-        switch (mySetting) {
-            case OFF:
-                mySetting = Setting.LOW;
-                break;
-            case LOW:
-                mySetting = Setting.MEDIUM;
-                break;
-            case MEDIUM:
-                mySetting = Setting.HIGH;
-                break;
-            case HIGH:
-                break;
-        }
-        timer = TIME_DURATION; // Resets timer
-    }
-	
-	
+	public void plusButton() { // Increases the setting
+		switch (mySetting) {
+		case OFF:
+			mySetting = Setting.LOW;
+			break;
+		case LOW:
+			mySetting = Setting.MEDIUM;
+			break;
+		case MEDIUM:
+			mySetting = Setting.HIGH;
+			break;
+		case HIGH:
+			break;
+		}
+		timer = TIME_DURATION; // Resets timer
+	}
+
+
+	public void minusButton() { // Decreases the setting
+		switch (mySetting) {
+		case HIGH:
+			mySetting = Setting.MEDIUM;
+			break;
+		case MEDIUM:
+			mySetting = Setting.LOW;
+			break;
+		case LOW:
+			mySetting = Setting.OFF;
+			break;
+		case OFF:
+			break;
+		}
+		timer = TIME_DURATION; // Resets timer
+	}
+
+
+	public void updateTemperature() { // Updates the temperature
+		if (timer > 0) {
+			timer--;
+		} else {
+			switch (mySetting) {
+			case HIGH:
+				myTemperature = Temperature.BLAZING;
+				break;
+			case MEDIUM:
+				myTemperature = Temperature.HOT;
+				break;
+			case LOW:
+				myTemperature = Temperature.WARM;
+				break;
+			case OFF:
+				myTemperature = Temperature.COLD;
+				break;
+			}
+		}
+	}
+
+
+
 
 }
