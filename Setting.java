64a89631b816a3ddfa157662876255ad2f1ecae2
katecallan <kate_callan@mines.edu>
2025-01-28 13:20:38 -0700

    Add Setting enum with string representations

---
+++ Setting.java
@@ -0,0 +1,25 @@
+
+public enum Setting {
+    OFF("---"),
+    LOW("--+"),
+    MEDIUM("-++"),
+    HIGH("+++");
+
+    private final String representation;
+
+    // Constructor that accepts a String
+    Setting(String representation) {
+        this.representation = representation;
+    }
+
+    // Getter for the string representation
+    public String getRepresentation() {
+        return representation;
+    }
+
+    // Override toString to return the string representation
+    @Override
+    public String toString() {
+        return representation;
+    }
+}
