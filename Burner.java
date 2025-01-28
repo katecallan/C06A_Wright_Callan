733db05b8f65f81dca8cc23094c46ccefff9c7e9
katecallan <kate_callan@mines.edu>
2025-01-28 13:16:51 -0700

    Initial commit

---
+++ Burner.java
@@ -0,0 +1,29 @@
+
+public class Burner {
+
+	// Temperature enumerated type and variable, with getter
+	
+	
+	
+	// Constructor
+	
+	
+	
+	// Plus button method and minus button method with switch statements
+	
+	
+	
+	// updateTemperature() method that correctly handles timer and increasing/decreasing temperature
+
+	
+	
+	// Display method
+
+	
+	
+	
+	
+	
+	
+}
+
