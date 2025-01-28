488a7c47a5a847f863f197d1c703973dd1daca6b
katecallan <kate_callan@mines.edu>
2025-01-28 13:25:57 -0700

    Added variables and Constructor

--- Burner.java
+++ Burner.java
@@ -1,10 +1,20 @@
 public class Burner{
-	
+
 	public enum Temperature { // Enum for temperature levels
-        BLAZING, HOT, WARM, COLD;
-    }
-	
-	
-	
-	
+		BLAZING, HOT, WARM, COLD;
+	}
+
+	public static final int TIME_DURATION = 2; // Constants and instance variables
+	private Temperature myTemperature;
+	private Setting mySetting;
+	private int timer;
+
+	public Burner() { // Constructor
+		this.myTemperature = Temperature.COLD;
+		this.mySetting = Setting.OFF;
+		this.timer = 0;
+	}
+
+
+
 }
